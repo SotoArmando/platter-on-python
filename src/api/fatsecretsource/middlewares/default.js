18a6797ee8029db4a7330d1812c321b04761686b
Sotoarmando <armando29@live.com>
2024-07-06 21:48:37 -0400

    updated time out

--- src/api/fatsecretsource/middlewares/default.js
+++ src/api/fatsecretsource/middlewares/default.js
@@ -1,12 +1,10 @@
 
 module.exports = (config, { strapi }) => async (context, next) => {
-  strapi.app.use(async (ctx, next) => {
-    // Your custom code here
-    setTimeout(() => {
-      return ctx.throw(408);
-    }, 4000);
+  // Your custom code here
+  setTimeout(() => {
+    return context.throw(408);
+  }, 4000);
 
-    // Continue to the next middleware or controller
-    await next();
-  });
+  // Continue to the next middleware or controller
+  await next();
 };
